9286d8d16958bafd5e491cda67dc6a7825531653
Rajesh <rajesh.s@technovert.com>
2020-05-09 10:23:42 +0530

    added same code base for both web and desktop applications

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DetailsModule } from './details/details.module';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DetailsModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
